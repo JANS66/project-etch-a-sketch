d63f5c5ac7a466f8cf04eca7a51723420d980c3d
JANS <98973687+JANS66@users.noreply.github.com>
2025-07-14 19:31:52 +0300

    feat: Create working button, for resizing the grid

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
-const container = document.querySelector("#container");
-
-for(let i = 0 ; i < 256; i++) {
-    const div = document.createElement("div");
-    div.id = `div${i}`;
-    container.appendChild(div);
-    
-    div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "blue";
-    })
-}
-
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-    let value = prompt("Enter a desired grid size from 1 to 100");
-})
+    let input = prompt("Enter a desired grid size from 1 to 100");
+    if (input === null) return;
+    let value = Number(input);
+    while (isNaN(value) || value < 1 || value > 100) {
+        input = prompt("Invalid input. Please enter a numeric value between 1 and 100");
+        if (input === null) return;
+        value = Number(input);
+    };
+    generateGrid(value);
+});
+
+const container = document.querySelector("#container");
+
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function generateGrid(size) {
+    clearGrid();
+
+    const totalDivs = size * size;
+    const squareSize = 100 / size;
+
+    for (let i = 0; i < totalDivs; i++) {
+        const div = document.createElement('div');
+        div.style.flex = `0 0 ${squareSize}%`;
+        div.style.aspectRatio = '1 / 1';
+        container.appendChild(div);
+
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = "blue";
+        })
+    }
+}
+
+generateGrid(16);
