6a6f17f53626c87b8f8f49754601ae55f6b8f708
JANS <98973687+JANS66@users.noreply.github.com>
2025-07-14 12:01:45 +0300

    fix: Move the container selection outside the loop

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+const container = document.querySelector("#container");
+
 for(let i = 0 ; i < 256; i++) {
     const div = document.createElement("div");
-    const container = document.querySelector("#container");
     div.id = `div${i}`;
     container.appendChild(div);
 }
