7c914bc2db677c139e530c590d3760a33acf33b8
JANS <98973687+JANS66@users.noreply.github.com>
2025-07-14 11:52:42 +0300

    refactor: Move grid squares inside container

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 for(let i = 0 ; i < 256; i++) {
     const div = document.createElement("div");
+    const container = document.querySelector("#container");
     div.id = `div${i}`;
-    document.body.appendChild(div);
+    container.appendChild(div);
 }
